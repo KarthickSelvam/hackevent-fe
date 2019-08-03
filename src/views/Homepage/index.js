8642d59feb1e40e275fc8bc2a45c1082897f81ba
Bhala Hariharan <bhala.vishnu@gmail.com>
2019-08-03 15:27:32 +0530

    Add background image

--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core';
+import image from '../../assets/img/background-image.jpg';
+
+const styles = () => ({
+  pageHeader: {
+    minHeight: '100vh',
+    height: 'auto',
+    display: 'inherit',
+    position: 'relative',
+    margin: '0',
+    padding: '0',
+    border: '0',
+    alignItems: 'center'
+  }
+});
 
 class Homepage extends Component {
   render() {
-    return <div>Homepage</div>;
+    return (
+      <div>
+        <div
+          className={this.props.classes.pageHeader}
+          style={{
+            backgroundImage: 'url(' + image + ')',
+            backgroundSize: 'cover',
+            backgroundPosition: 'top center'
+          }}
+        />
+      </div>
+    );
   }
 }
 
-export default Homepage;
+Homepage.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(styles)(Homepage);
