38bb1bf5ac93ccf596d0fbb0d2006038aab660a0
Bhala Hariharan <bhala.vishnu@gmail.com>
2019-08-03 16:23:01 +0530

    Add cards to display the games

--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, Grid, Typography, withStyles } from '@material-ui/core';
 import image from '../../assets/img/background-image.jpg';
 
 const styles = () => ({
@@ -13,21 +13,62 @@ const styles = () => ({
     padding: '0',
     border: '0',
     alignItems: 'center'
-  }
+  },
+  card: {
+    width: 275,
+    margin: 10,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
 });
 
 class Homepage extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <div
-          className={this.props.classes.pageHeader}
+          className={classes.pageHeader}
           style={{
             backgroundImage: 'url(' + image + ')',
             backgroundSize: 'cover',
-            backgroundPosition: 'top center'
+            backgroundPosition: 'top center',
+            display: 'flex',
           }}
-        />
+        >
+          <Grid container justify="center">
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography className={classes.title} color="textPrimary" component="p" gutterBottom variant="body2">
+                  Pronunciation Checker
+                </Typography>
+                <Typography component="p" variant="body2">
+                  A two player game where you will be spelling out the sentence that is displayed. A score will be given to you on the basis of your pronunciation accuracy.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Play Now</Button>
+              </CardActions>
+            </Card>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography className={classes.title} color="textPrimary" component="p" gutterBottom variant="body2">
+                  Word War
+                </Typography>
+                <Typography component="p" variant="body2">
+                  A two player game where you will be given with a word and you should find the synonym and your partner should find the antonym of the word. A score will be given on the basis of correct answer.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Play Now</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </div>
       </div>
     );
   }
