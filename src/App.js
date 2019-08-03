1188d08c57f5f883cbc5dcdc977ea504fbf8a864
Bhala Hariharan <bhala.vishnu@gmail.com>
2019-08-03 17:12:39 +0530

    Add socket.io-client and socket.io-react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import io from 'socket.io-client';
+import { SocketProvider } from 'socket.io-react';
 import { ThemeProvider } from '@material-ui/styles';
 import { Homepage, NotFound } from './views';
+
+const socket = io.connect('http://localhost:3001/');
 const browserHistory = createBrowserHistory();
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider>
-        <Router history={browserHistory}>
-          <Switch>
-            <Route component={Homepage} exact path="/" />
-            <Route component={NotFound} exact path="/not-found" />
-            <Redirect to="/not-found" />
-          </Switch>
-        </Router>
-      </ThemeProvider>
+      <SocketProvider socket={socket}>
+        <ThemeProvider>
+          <Router history={browserHistory}>
+            <Switch>
+              <Route component={Homepage} exact path="/" />
+              <Route component={NotFound} exact path="/not-found" />
+              <Redirect to="/not-found" />
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </SocketProvider>
     );
   }
 }
